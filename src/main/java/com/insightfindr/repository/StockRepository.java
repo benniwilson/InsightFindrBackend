970e1d5de84b37bbfab65eaa9e8c856d45694bcf
benniwilson <benniwilson7@gmail.com>
2025-03-17 16:25:00 +0000

    feat: Added the repository annotation to the StockRepository interface

--- src/main/java/com/insightfindr/repository/StockRepository.java
+++ src/main/java/com/insightfindr/repository/StockRepository.java
@@ -2,6 +2,8 @@ package com.insightfindr.repository;
 
 import com.insightfindr.model.Stock;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockRepository extends CrudRepository<Stock, Long> {
 }
