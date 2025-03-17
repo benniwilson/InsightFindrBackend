a4553fd1d19a3938c9b50d48f88e7c23c0c31d82
benniwilson <benniwilson7@gmail.com>
2025-03-17 12:20:09 +0000

    created a stock class with the relevant attributes and annotations, still need to add the data mapping for the person class to the stock class

---
+++ src/main/java/com/insightfindr/model/Stock.java
@@ -0,0 +1,48 @@
+package com.insightfindr.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Getter
+@Setter
+public class Stock {
+
+    public Stock() {
+    }
+
+    public Stock(long id, String ticker, String name, String message, int invested, String date_invested) {
+        this.id = id;
+        this.ticker = ticker;
+        this.name = name;
+        this.message = message;
+        this.invested = invested;
+        this.date_invested = date_invested;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, unique = true)
+    private long id;
+
+    @Column(name = "ticker", nullable = false)
+    private String ticker;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "amount_invested")
+    private int invested;
+
+    @Column(name = "date_invested")
+    private String date_invested;
+}
