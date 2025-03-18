477c7bce91458e1f36bad635a68c17c66991fa31
benniwilson <benniwilson7@gmail.com>
2025-03-18 11:58:45 +0000

    feat: Added the required methods to the PersonSerice interface

--- src/main/java/com/insightfindr/service/PersonService.java
+++ src/main/java/com/insightfindr/service/PersonService.java
@@ -1,5 +1,14 @@
 package com.insightfindr.service;
 
+import com.insightfindr.model.Person;
+import com.insightfindr.model.Stock;
+
+import java.util.List;
+
 public interface PersonService {
     //Add all the required functions here.
+    public Person getPersonById(String id) throws Exception; //Once the person has been retrieved you can get the list of stocks.
+    public Person postPerson(Person person);
+    public Person putPerson(Person person, String id); //This can be used to add the stocks to the Person.
+    public Person deletePerson(String id);
 }
