f9b6eaa04d81b0a0311b6bebcadc2862f1a0bb68
benniwilson <benniwilson7@gmail.com>
2025-03-18 12:03:09 +0000

    feat: Implemented the new methods from the PersonService, also added implementation for the getPersonById method

--- src/main/java/com/insightfindr/service/PersonServiceImpl.java
+++ src/main/java/com/insightfindr/service/PersonServiceImpl.java
@@ -1,12 +1,40 @@
 package com.insightfindr.service;
 
+import com.insightfindr.model.Person;
+import com.insightfindr.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PersonServiceImpl implements PersonService {
 
     @Autowired
-    PersonService personService;
+    PersonRepository repository;
 
+    @Override
+    public Person getPersonById(String id) throws Exception {
+        Optional<Person> person = repository.findById(id);
+        if (person.isPresent()){
+            return person.get();
+        }else {
+            throw new Exception(String.format("No Album with id: %s, was found in the system", id));
+        }
+    }
+
+    @Override
+    public Person postPerson(Person person) {
+        return null;
+    }
+
+    @Override
+    public Person putPerson(Person person, String id) {
+        return null;
+    }
+
+    @Override
+    public Person deletePerson(String id) {
+        return null;
+    }
 }
