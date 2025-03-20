e93e5ad6020973e0ba782074f502b47d5d722bda
benniwilson <benniwilson7@gmail.com>
2025-03-20 11:43:43 +0000

    refactor: Got rid of the physical getters and setters as the lombok annotations are now fixed

--- src/main/java/com/insightfindr/model/Person.java
+++ src/main/java/com/insightfindr/model/Person.java
@@ -28,36 +28,4 @@ public class Person {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_stock_id", referencedColumnName = "user_id")
     private List<Stock> stock;
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public List<Stock> getStock() {
-        return stock;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setStock(List<Stock> stock) {
-        this.stock = stock;
-    }
 }
